59e33288f5a6ab2db5f98c9b8c9a7db6247bc4df
cashvini <chanu88ashvini@gmail.com>
2026-09-01 17:45:37 +0200

    Initial Spring Boot project setup with database connection

---
+++ src/main/java/org/example/crochetbusiness/CrochetBusinessApplication.java
@@ -0,0 +1,13 @@
+package org.example.crochetbusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrochetBusinessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrochetBusinessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/crochetbusiness/CrochetBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.crochetbusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrochetBusinessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
